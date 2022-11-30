275570d4453ce39f811597da26f8379eb34d3817
Christina Antonuccio <antonuccio.christina@gmail.com>
2022-11-30 16:13:06 -0600

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -4,5 +4,11 @@ function helloWorld() {
 }
 
 function sayHello(input) {
+    if (typeof(input) === "string") {
         return "Hello, " + input + "!";
+    } else if (input === true || input === false) {
+        return "Hello, World!";
+    } else {
+        return "Hello, World!";
+    }
 }
